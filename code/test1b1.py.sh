b983e7716f4507458303145ba2885d3e592e0a87
hmiura <hmiura@eosloan.inband>
2022-08-10 16:24:01 -0400

    Push latest updates.

--- code/test1b1.py.sh
+++ code/test1b1.py.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=1G
 #SBATCH --partition=sched_mit_sloan_interactive
-#SBATCH --time=0-01:00
+#SBATCH --time=0-00:10
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=hmiura@mit.edu
 #SBATCH --output=/dev/null
@@ -12,16 +12,16 @@
 
 # Set program name.
 program="test1"
-version="a1"
+version="b1"
 # Activate environment.
-source activate testrenv
+source activate testpyenv
 
 # Set input parameters.
-params='list(
-"test"=0
-)'
+params="{
+'test':0
+}"
 
 # Execute script.
-Rscript --verbose ${program}${version}.R \
-${program} ${version} "${params}" \
+python ${program}${version}.py \
+"${params}" \
 > ../output/logs/${program}${version}_${SLURM_JOB_ID}.log 2>&1
